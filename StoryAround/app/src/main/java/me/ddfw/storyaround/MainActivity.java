712e7ffdb7bdf124ec59c5cf4ee91a0b07e794bb
annie823 <annie823123@hotmail.com>
2017-02-25 20:03:40 -0500

    main activity fragment outline
    few lines about the main fragment

--- StoryAround/app/src/main/java/me/ddfw/storyaround/MainActivity.java
+++ StoryAround/app/src/main/java/me/ddfw/storyaround/MainActivity.java
@@ -4,6 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    // FragmentStart
+    // FragmentLikes
+    // FragmentPost
+    // FragmentDiary
+    // FragmentUser
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
