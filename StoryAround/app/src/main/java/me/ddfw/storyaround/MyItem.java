04867331cc52e35a4507758223985e73e00c451b
annie823 <annie823123@hotmail.com>
2017-03-05 21:42:22 -0500

    Revert "Revert "j""
    This reverts commit 80e80d40dc2c3808023d3d4e857764b6b62cf458.

---
+++ StoryAround/app/src/main/java/me/ddfw/storyaround/MyItem.java
@@ -0,0 +1,36 @@
+package me.ddfw.storyaround;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterItem;
+
+/**
+ * Created by apple on 2017/3/5.
+ */
+
+public class MyItem implements ClusterItem {
+    private final LatLng mPosition;
+    private final String mTitle;
+    private final String mSnippet;
+
+
+    public MyItem(double lat, double lng, String title, String snippet) {
+        mPosition = new LatLng(lat, lng);
+        mTitle = title;
+        mSnippet = snippet;
+    }
+
+    @Override
+    public LatLng getPosition() {
+        return mPosition;
+    }
+
+    @Override
+    public String getTitle() {
+        return mTitle;
+    }
+
+    @Override
+    public String getSnippet() {
+        return mSnippet;
+    }
+}
