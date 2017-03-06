a182eb14ea2a7a4a9cc0cd07dbae25149e554b89
annie823 <annie823123@hotmail.com>
2017-03-05 21:08:42 -0500

    post fragment UI update

--- StoryAround/app/src/main/java/me/ddfw/storyaround/fragments/PostFragment.java
+++ StoryAround/app/src/main/java/me/ddfw/storyaround/fragments/PostFragment.java
@@ -1,51 +1,17 @@
 package me.ddfw.storyaround.fragments;
 
-import android.app.Activity;
 import android.app.Fragment;
-import android.app.Service;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.Rect;
-import android.graphics.drawable.GradientDrawable;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.text.Editable;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
-import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RelativeLayout;
-import android.widget.TableLayout;
-import android.widget.Toast;
-
-import com.github.irshulx.BaseClass;
-import com.github.irshulx.Editor;
-
-import me.ddfw.storyaround.MainActivity;
 import me.ddfw.storyaround.NewStoryActivity;
 import me.ddfw.storyaround.R;
-import okhttp3.OkHttpClient;
-import retrofit2.Retrofit;
-
-import com.github.irshulx.models.EditorTextStyle;
-
-import java.io.IOException;
-import java.util.Calendar;
 
 public class PostFragment extends Fragment{
-
-    private Editor mEditor;
-    private TableLayout tabLayout;
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -71,8 +37,6 @@ public class PostFragment extends Fragment{
 
         return rootView;
     }
-
-
 
     public void onStartClicked() {
         Intent intent;
